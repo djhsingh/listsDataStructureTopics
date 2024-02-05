f245b2c5ee1c557876158ef98ad3b04576467d2f
djhsingh <djhsingh0001@gmail.com>
2024-02-05 22:11:14 +0530

    ArrayList

--- src/main/java/djh/learn/listsDS/ArrayListDS.java
+++ src/main/java/djh/learn/listsDS/ArrayListDS.java
@@ -1,7 +1,26 @@
 package djh.learn.listsDS;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArrayListDS {
     public static void main(String[] args) {
+        List<Employee> employeeList = new ArrayList<>();
+        employeeList.add(new Employee("John","Adam",53533));
+        employeeList.add(new Employee("Timm","burton",3433));
+        employeeList.add(new Employee("Paul","Geferry",8777));
+        employeeList.add(new Employee("Hanna","Montana",77575));
+
+        System.out.println(employeeList);
+        employeeList.add(2,new Employee("Rahul","Sharma",1234));
+        System.out.println(employeeList);
+
+        employeeList.set(1,new Employee("Timber","lake",122334));
+        System.out.println(employeeList);
+
+        System.out.println(employeeList.isEmpty());
+        System.out.println(employeeList.size());
+        System.out.println(employeeList.stream().anyMatch(e -> e.getFirstName().equalsIgnoreCase("Timm")));
 
     }
 }
---
+++ src/main/java/djh/learn/listsDS/Employee.java
@@ -0,0 +1,46 @@
+package djh.learn.listsDS;
+
+public class Employee {
+    private String firstName;
+    private String secondName;
+    private int salary;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public Employee(String firstName, String secondName, int salary) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", secondName='" + secondName + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
